d9689f6e2cbad0592bf84ba960bba29405b74f02
Abanoub bashara <156399921+abanoub-bashara@users.noreply.github.com>
2026-02-12 23:47:07 -0500

    Delete build directory

--- build/delivery_bot_nav/CMakeFiles/delivery_bot_nav_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/delivery_bot_nav_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/delivery_bot_nav_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
